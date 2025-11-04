a423c065edcd4f53b7226e9295d91225789ccbab
MSandobie <imaginesmtyt@gmail.com>
2025-11-04 19:29:17 +0800

    Add project files.

---
+++ LabExe10/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.ButtonWrite = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ButtonREAD = New System.Windows.Forms.Button()
+        Me.ButtonSort = New System.Windows.Forms.Button()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(231, 116)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a number: "
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(322, 111)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 1
+        '
+        'ButtonWrite
+        '
+        Me.ButtonWrite.Location = New System.Drawing.Point(466, 111)
+        Me.ButtonWrite.Name = "ButtonWrite"
+        Me.ButtonWrite.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonWrite.TabIndex = 2
+        Me.ButtonWrite.Text = "WRITE"
+        Me.ButtonWrite.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(322, 156)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 3
+        '
+        'ButtonREAD
+        '
+        Me.ButtonREAD.Location = New System.Drawing.Point(466, 140)
+        Me.ButtonREAD.Name = "ButtonREAD"
+        Me.ButtonREAD.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonREAD.TabIndex = 4
+        Me.ButtonREAD.Text = "READ"
+        Me.ButtonREAD.UseVisualStyleBackColor = True
+        '
+        'ButtonSort
+        '
+        Me.ButtonSort.Location = New System.Drawing.Point(466, 169)
+        Me.ButtonSort.Name = "ButtonSort"
+        Me.ButtonSort.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonSort.TabIndex = 5
+        Me.ButtonSort.Text = "SORT"
+        Me.ButtonSort.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ButtonSort)
+        Me.Controls.Add(Me.ButtonREAD)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ButtonWrite)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents ButtonWrite As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ButtonREAD As Button
+    Friend WithEvents ButtonSort As Button
+End Class
---
+++ LabExe10/Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim filePath As String = "file.text"
+    Private Sub ButtonWrite_Click(sender As Object, e As EventArgs) Handles ButtonWrite.Click
+
+        Try
+            Using Writer As New StreamWriter(filePath, True)
+                While 
+            End Using
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
